5bcb789d7269cfd99d91f98fc9a442a489960892
Chris Wilcox <crwilcox@google.com>
2019-08-05 10:42:12 +1000

    add test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -7,5 +7,14 @@ from packaging.metadata import Metadata
 
 class TestMetadata:
     def test_validate(self):
-        m = Metadata(filename='../test_data/mudpy-0.0.1.dev232.tar.gz')
+        m = Metadata(filename='test_data/mudpy-0.0.1.dev232.tar.gz')
         m.validate()
+
+    def test_invalid_license_text(self):
+        # this has an invalid (multiline) summary. It also has a multiline license tha tisn't proerply
+        # indented.
+        # Suggest making a runner that makes these packages so
+        # we can automate test data? for now coul have multiple zips though
+        m = Metadata(filename='test_data/test-package-0.1.tar.gz')
+        m.validate()
+
